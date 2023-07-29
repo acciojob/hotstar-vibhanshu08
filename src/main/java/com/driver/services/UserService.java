832fa44d23754b34806478e591061f0fbc9472a8
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-07-29 21:41:38 +0530

    Update UserService.java

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -25,16 +25,28 @@ public class UserService {
     public Integer addUser(User user){
 
         //Jut simply add the user to the Db and return the userId returned by the repository
-        return null;
+        user = userRepository.save(user);
+        return user.getId();
     }
 
     public Integer getAvailableCountOfWebSeriesViewable(Integer userId){
 
         //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
         //Hint: Take out all the Webseries from the WebRepository
+        User user =userRepository.findById(userId).get();
+        Integer age=0;
+        if(user.getAge()<=18)age=18;
+        else age=Integer.MAX_VALUE;
+        List<WebSeries> webSeriesList= webSeriesRepository.findAll();
+
+        Integer count=0;
+
+        for(WebSeries webSeries:webSeriesList){
+            if(webSeries.getAgeLimit()<=age)count++;
+        }
 
 
-        return null;
+        return count;
     }
 
 
