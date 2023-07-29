24e02276bae7ff7fd18a04507f8dd0610cc1b02d
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-07-29 21:55:38 +0530

    Create WebSeriesTransformer.java

---
+++ src/main/java/com/driver/WebSeriesTransformer.java
@@ -0,0 +1,17 @@
+package com.driver.Transformer;
+
+import com.driver.EntryDto.WebSeriesEntryDto;
+import com.driver.model.WebSeries;
+
+public class WebSeriesTransformer {
+    public static WebSeries convertDtoToEntity(WebSeriesEntryDto entryDto){
+
+        WebSeries webSeries=new WebSeries();
+        webSeries.setSeriesName(entryDto.getSeriesName());
+        webSeries.setAgeLimit(entryDto.getAgeLimit());
+        webSeries.setRating(entryDto.getRating());
+        webSeries.setSubscriptionType(entryDto.getSubscriptionType());
+
+        return webSeries;
+    }
+}
