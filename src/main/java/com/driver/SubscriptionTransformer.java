d838457822607af5d6ab7fdf1531b4c3c4c8d4b8
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-07-29 21:54:54 +0530

    Update SubscriptionTransformer.java

--- src/main/java/com/driver/SubscriptionTransformer.java
+++ src/main/java/com/driver/SubscriptionTransformer.java
@@ -1,1 +1,14 @@
+package com.driver.Transformer;
 
+import com.driver.EntryDto.SubscriptionEntryDto;
+import com.driver.model.Subscription;
+
+public class SubscriptionTransformer {
+    public static Subscription convertDtoToEntity(SubscriptionEntryDto entryDto) {
+        Subscription subscription = new Subscription();
+        subscription.setSubscriptionType(entryDto.getSubscriptionType());
+        subscription.setNoOfScreensSubscribed(entryDto.getNoOfScreensRequired());
+
+        return subscription;
+    }
+}
